74132c4eb9aacfbf577890011fc7f5cb507a6529
dyk6208 <dyk6208@gmail.com>
2024-01-27 22:56:50 +0900

    db 연결

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://dyk6208:1234@localhost/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # connect_args는 SQLite에만 필요
+    SQLALCHEMY_DATABASE_URL, 
+    # connect_args={"check_same_thread": False} # connect_args는 SQLite에만 필요
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
