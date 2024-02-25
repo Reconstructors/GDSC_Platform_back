b6481decceeda075ec931aa1fbde0dca8a9aa2b7
dyk6208 <dyk6208@gmail.com>
2024-02-25 16:03:44 +0900

    파일 구조 변경 중

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.event import event_router
+from domain.intro import intro_router
+from domain.notice import notice_router
+from domain.project import project_router
+from domain.study import study_router
+from domain.user import user_router
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"    # 또는 "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(event_router.router)
+app.include_router(intro_router.router)
+app.include_router(notice_router.router)
+app.include_router(project_router.router)
+app.include_router(study_router.router)
+app.include_router(user_router.router)
---
+++ models.py
@@ -0,0 +1,125 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON, DateTime, Enum, Date, Text
+from sqlalchemy.orm import relationship
+from enum import Enum as PyEnum
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    cohort = Column(Integer, index=True)  # 동아리 기수
+    position = Column(String, index=True) # core, member, leader
+    bio = Column(String)  # 한 줄 소개
+    description = Column(Text)  # 자세한 자기소개
+    links = Column(JSON)  # JSON 형태의 문자열로 저장
+    skills = Column(JSON)  # 스킬셋, list 타입으로 저장
+    interests = Column(JSON)  # 관심분야, list 타입으로 저장
+    project_interest = Column(Boolean)  # 프로젝트 참여 희망 여부
+
+# 상태를 나타내는 Enum 클래스 정의
+class ProjectStatus(PyEnum):
+    PLANNING = "Planning"
+    ONGOING = "Ongoing"
+    COMPLETED = "Completed"
+    PAUSED = "Paused"
+    CANCELED = "Canceled"
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)  # 프로젝트 제목
+    start = Column(Date)  # 프로젝트 시작 시간
+    end = Column(Date, nullable=True)  # 프로젝트 종료 시간, null 허용
+    description = Column(Text)  # 프로젝트 소개
+    contact_info = Column(JSON)  # 연락처 정보, JSON 형태의 문자열로 저장
+    status = Column(String, Enum(ProjectStatus))  # 스터디 상태
+    photo_ids = Column(JSON)  # 사진 ID 리스트, JSON 타입으로 저장
+
+class UserProjectMatch(Base):
+    __tablename__ = "user_project_match"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('user.id'))  # 유저 ID, 유저 테이블에 대한 외래 키
+    project_id = Column(Integer, ForeignKey('project.id'))  # 스터디 ID, 스터디 테이블에 대한 외래 키
+    is_approved = Column(Boolean, default=False)  # 승인 여부, 기본값은 False
+    is_leader = Column(Boolean, default=False)  # 팀장 여부, 기본값은 False
+
+    user = relationship("User", backref="projects")
+    study = relationship("Project", backref="users")
+
+
+class Study(Base):
+    __tablename__ = "study"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)  # 스터디 제목
+    start = Column(DateTime)  # 스터디 시작 시간
+    end = Column(DateTime, nullable=True)  # 스터디 종료 시간, null 허용
+    description = Column(String)  # 스터디 소개
+    contact_info = Column(JSON)  # 연락처 정보, JSON 형태의 문자열로 저장
+    status = Column(String)  # 스터디 상태
+    photo_ids = Column(JSON)  # 사진 ID 리스트, JSON 타입으로 저장
+
+class UserStudyMatch(Base):
+    __tablename__ = "user_study_match"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('user.id'))  # 유저 ID, 유저 테이블에 대한 외래 키
+    study_id = Column(Integer, ForeignKey('study.id'))  # 스터디 ID, 스터디 테이블에 대한 외래 키
+    is_approved = Column(Boolean, default=False)  # 승인 여부, 기본값은 False
+    is_leader = Column(Boolean, default=False)  # 스터디 팀장 여부, 기본값은 False
+
+    user = relationship("User", backref="studies")
+    study = relationship("Study", backref="users")
+
+
+class Notice(Base):
+    __tablename__ = "notice"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True) 
+    tag = Column(JSON)
+    upload_date = Column(Date)
+    view = Column(Integer)
+    photo_ids = Column(JSON)
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey("user"))
+    user = relationship("User", backref="notice_users")
+    modify_date = Column(DateTime, nullable=True)
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    start = Column(DateTime)  # 스터디 시작 시간
+    end = Column(DateTime, nullable=True)  # 스터디 종료 시간, null 허용
+    status = Column(String)  # Done ongoing upcoming
+    type = Column(String)  # Regular, General,In campus, out campus
+    photo_id = Column(String)
+    location = Column(String)
+    user_id = Column(Integer, ForeignKey("user"))
+    user = relationship("User", backref="event_users")
+    modify_date = Column(DateTime, nullable=True)
+
+
+class Timeline(Base):
+    __tablename__ = "timeline"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)  # 활동 제목
+    date = Column(Date)  # 활동 일자
+
+
+
+
+
+
+
+
+
